ee8cfc304f917cef576cba846519683035cfd2d1
Eugenio <eugi90@gmail.com>
2016-11-21 17:25:30 +0100

    Bump version to 1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 
 setup(name='scatnetgpu',
-      version='0.1.3',
+      version='1.0',
       description='Scattering Network for Python and CUDA',
       long_description=readme(),
       url='http://github.com/oinegue/scatnetgpu',
